f34e9518ea7ad0c295d92f4d37fc227b766a3e90
IlyaAnufriev <ilya.anufriev2014@yandex.ru>
2025-12-29 12:30:44 +0300

    Initial Commit

---
+++ TeslaAppProject/TeslaAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaAppProjectApp.swift
+//  TeslaAppProject
+//
+//  Created by Илья Ануфриев on 29.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TeslaAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
